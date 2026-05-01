a14c687b88f3a1d1f54a55a7f94473982dd6ec5d
Kuntala Eshwarsai <eshwarsaikuntala@gmail.com>
2026-05-02 03:30:14 +0530

    connected railway db

--- db.py
+++ db.py
@@ -1,12 +1,16 @@
-import os
 import mysql.connector
+import os
+from urllib.parse import urlparse
 
 def get_db_connection():
-    conn = mysql.connector.connect(
-        host=os.getenv("MYSQLHOST"),
-        user=os.getenv("MYSQLUSER"),
-        password=os.getenv("MYSQLPASSWORD"),
-        database=os.getenv("MYSQLDATABASE"),
-        port=int(os.getenv("MYSQLPORT"))
+    url = os.getenv("DATABASE_URL")
+
+    parsed = urlparse(url)
+
+    return mysql.connector.connect(
+        host=parsed.hostname,
+        user=parsed.username,
+        password=parsed.password,
+        database=parsed.path.lstrip('/'),
+        port=parsed.port
     )
-    return conn
