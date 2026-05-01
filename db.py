11d3891f3a865ab46c4ae09e3476823a9fd50e7c
Kuntala Eshwarsai <eshwarsaikuntala@gmail.com>
2026-05-02 03:20:01 +0530

    Connected Railway MySQL

--- db.py
+++ db.py
@@ -1,9 +1,12 @@
+import os
 import mysql.connector
 
 def get_db_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Eshwar#bittu30",
-        database="team_task_manager"
+    conn = mysql.connector.connect(
+        host=os.getenv("MYSQLHOST"),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE"),
+        port=int(os.getenv("MYSQLPORT"))
     )
+    return conn
